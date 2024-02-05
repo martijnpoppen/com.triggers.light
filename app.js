1eab44c3f29d4ccfc4e9934508a3cbf7163bc4ba
Martijn Poppen <m.poppen@ziggo.nl>
2024-02-05 10:03:42 +0100

    NEW: add any flowcard. Allowing to use a single card for multiple triggers

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Homey = require('homey');
 const { HomeyAPI } = require('homey-api');
 
 const flowTriggers = require('./lib/flows/triggers');
+const flowActions = require('./lib/flows/actions');
 
 class App extends Homey.App {
     log() {
@@ -24,45 +25,76 @@ class App extends Homey.App {
             debug: false
         });
 
+        this.DEVICES = Object.values(await this._api.devices.getDevices());
+        this.ONOFF_DEVICES = [];
+
         await flowTriggers.init(this);
-        await this.setCheckZoneOnOffInterval();
+        await flowActions.init(this);
+        await this.setDeviceListeners();
     }
 
     // -------------------- FUNCTIONS ----------------------
 
-    async setCheckZoneOnOffInterval() {
-        const devices = Object.values(await this._api.devices.getDevices());
+    async setDeviceListeners() {
+        this.error(`[Device][setDeviceListeners]`, 'Setting device listeners');
 
-        for (const device of devices) {
-            if (device.class === 'light' || (device.virtualClass === 'light' && device.capabilitiesObj)) {
-                if (device.capabilitiesObj.light_temperature) {
-                    device.makeCapabilityInstance('light_temperature', (value) => {
-                        this.homey.app.trigger_DEVICE_TEMP
-                            .trigger({ name: device.name, id: device.id, value: value }, { device: device.id })
-                            .catch(err => this.error(`[Device][trigger_DEVICE_TEMP]`, err))
-                            .then(this.log(`[trigger_DEVICE_TEMP] - Triggered - ${device.name} - ${value}`));
-                    });
-                }
+        for (const device of this.DEVICES) {
+            try {
+                if (device.class === 'light' || (device.virtualClass === 'light' && device.capabilitiesObj)) {
+                    if (device.capabilitiesObj && device.capabilitiesObj.light_temperature) {
+                        device.makeCapabilityInstance('light_temperature', (value) => {
+                            this.triggerFlow(device, 'light_temperature', value, 'DEVICE_TEMP');
+                        });
+                    }
 
-                if (device.capabilitiesObj.light_saturation) {
-                    device.makeCapabilityInstance('light_saturation', (value) => {
-                        this.homey.app.trigger_DEVICE_SATURATION
-                            .trigger({ name: device.name, id: device.id, value: value }, { device: device.id })
-                            .catch(err => this.error(`[Device][trigger_DEVICE_SATURATION]`, err))
-                            .then(this.log(`[trigger_DEVICE_SATURATION] - Triggered - ${device.name} - ${value}`));
-                    });
-                }
+                    if (device.capabilitiesObj && device.capabilitiesObj.light_saturation) {
+                        device.makeCapabilityInstance('light_saturation', (value) => {
+                            this.triggerFlow(device, 'light_saturation', value, 'DEVICE_SATURATION');
+                        });
+                    }
 
-                if (device.capabilitiesObj.light_hue) {
-                    device.makeCapabilityInstance('light_hue', (value) => {
-                        this.homey.app.trigger_DEVICE_HUE
-                            .trigger({ name: device.name, id: device.id, value: value }, { device: device.id })
-                            .catch(err => this.error(`[Device][trigger_DEVICE_HUE]`, err))
-                            .then(this.log(`[trigger_DEVICE_HUE] - Triggered - ${device.name} - ${value}`));
-                    });
+                    if (device.capabilitiesObj && device.capabilitiesObj.light_hue) {
+                        device.makeCapabilityInstance('light_hue', (value) => {
+                            this.triggerFlow(device, 'light_hue', value, 'DEVICE_HUE');
+                        });
+                    }
+
+                    if (device.capabilitiesObj && device.capabilitiesObj.onoff) {
+                        device.makeCapabilityInstance('onoff', (value) => {
+                            this.triggerFlow(device, 'onoff', value);
+                        });
+                    }
+
+                    if (device.capabilitiesObj && device.capabilitiesObj.dim) {
+                        device.makeCapabilityInstance('dim', (value) => {
+                            this.triggerFlow(device, 'dim', value);
+                        });
+                    }
                 }
+            } catch (error) {
+                this.error(`[Device][setDeviceListeners]`, error);
             }
         }
+    }
+
+    triggerFlow(device, capability, value, flowName = null) {
+        this.log(`[Device][triggerFlow]`, device.name, capability, value);
+
+        if (flowName) {
+            this.homey.app.trigger_[flowName]
+                .trigger({ name: device.name, id: device.id, value: value, capability }, { name: device.name, id: device.id, value: value, device: device, capability })
+                .catch((err) => this.error(`[Device][trigger_${flowName}]`, err))
+                .then(this.log(`[trigger_DEVICE_HUE] - Triggered - ${device.name} - ${value}`));
+        }
+
+        if(capability === 'onoff') {
+            value = Number(value);
+        }
+
+        this.homey.app.trigger_DEVICE_ANY
+            .trigger({ name: device.name, id: device.id, value: value, capability }, { name: device.name, id: device.id, value: value, device: device, capability })
+            .catch((err) => this.error(`[Device][trigger_DEVICE_ANY]`, err))
+            .then(this.log(`[trigger_DEVICE_ANY] - Triggered - ${device.name} - ${value}`));
     }
 }
 
